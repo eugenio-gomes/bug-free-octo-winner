18304dd7cffec9c0b68a9a66ab706a696bec4c64
Eugenio Gomes <eugeniogomess668@gmail.com>
2024-06-08 02:57:41 +0100

    first commit-commiting all files to the repository

---
+++ .vscode/tree.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int age;
+string "name";
+
+cout<<"enter your name: ";
+cin>>"name";
+cout<<"enter your age "<<endl;
+cin>>age;
+cout<<name<< " "<< age<<endl;
+    return 0;
+}

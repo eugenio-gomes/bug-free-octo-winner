8d60cccdb1fc8f4148f30315b7b08c0f3e7cb3c0
eugenio-gomes <eugeniogomes668@gmail.com>
2024-06-27 14:29:15 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 import unittest
-from emergency_management import Emergency, FireEmergency, Explosion, NaturalDisaster, Earthquake, Storm, EmergencyManager
+from Emergency.py import Emergency, FireEmergency, Explosion, NaturalDisaster, Earthquake, Storm, EmergencyManager
 
 class TestEmergency(unittest.TestCase):
 
